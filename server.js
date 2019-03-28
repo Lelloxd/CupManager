436cec7b6d02e7a99771cbfe0c84b3187c8384aa
Enrico Zamberlan <enrico.zamberlan@h-farm.com>
2019-03-28 17:06:31 +0100

    added express dependency

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+console.log(path.join(__dirname, 'www/dist'))
+app.use(serveStatic(path.join(__dirname, 'www/dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
