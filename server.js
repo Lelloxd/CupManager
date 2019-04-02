6cf07ce85b5e33e7fe53109787960180eee3fc6f
Enrico Zamberlan <enrico.zamberlan@h-farm.com>
2019-04-02 17:53:09 +0200

    console log

--- server.js
+++ server.js
@@ -5,4 +5,5 @@ app = express();
 console.log(path.join(__dirname, 'www/dist'))
 app.use(serveStatic(path.join(__dirname, 'www/dist')));
 const port = process.env.PORT || 80;
+console.log("port:",port)
 app.listen(port);
