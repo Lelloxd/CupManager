2f850f6eaa0d869c6118edaa28de366b204ea9ae
Enrico Zamberlan <enrico.zamberlan@h-farm.com>
2019-04-17 12:48:51 +0200

    fix server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const serveStatic = require("serve-static")
+var history = require('connect-history-api-fallback');
 const path = require('path');
 app = express();
 console.log(path.join(__dirname, 'www/dist'))
+app.use(history());
 app.use(serveStatic(path.join(__dirname, 'www/dist')));
 const port = process.env.PORT || 80;
 console.log("port:",port)
