df62790ae1392eee021ebf61150510c784b942f9
Enrico Zamberlan <enrico.zamberlan@h-farm.com>
2019-04-23 14:50:05 +0200

    Login page

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,19 +1,33 @@
 <template>
-  <div>
-    <h2>Login Page</h2>
-    <form @submit.prevent="handleSubmit">
-      <div>
-          <input placeholder="Your email" type="email" v-model="username">
+  <div class="component min-h-100">
+    <div class="col-md-4" v-if="!loading">
+      <div class="content">
+        <div class="mt-5">
+          <b-card>
+            <b-card-text>Login</b-card-text>
+
+            <form @submit.prevent="handleSubmit">
+              <div style="margin-bottom: 10px;">
+                <input placeholder="Your email" type="email" v-model="username">
+              </div>
+              <div style="margin-bottom: 10px;">
+                <input placeholder="Password" type="password" v-model="password">
+              </div>
+              <div style="margin-bottom: 10px;">
+                <button>Login</button>
+              </div>
+            </form>
+          </b-card>
+        </div>
       </div>
-      <div>
-          <input placeholder="Password" type="password" v-model="password">
-      </div>
-      <div>
-          <button>Login</button>
-      </div>
-    </form>
+    </div>
+    <div class="loading" v-if="loading">
+      <b-spinner variant="primary" label="spinning"></b-spinner>
+    </div>
   </div>
 </template>
 
+
 <script src="./Login.js"></script>
+<style src="../../styles/component.scss" lang="scss"></style>
 
